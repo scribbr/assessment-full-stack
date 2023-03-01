6b03bb73f0dcb51766dc661f4641843c285478af
Sven Hanssen <sven@svenhanssen.nl>
2023-03-01 14:51:39 +0100

    Initialize the assessments boilerplate

---
+++ workspaces/client/src/App.tsx
@@ -0,0 +1,13 @@
+import { APP_TITLE } from "@scribbr-assessment-full-stack/common";
+import React, { useState } from "react";
+
+import "./index.css";
+
+export function App() {
+  return (
+    <div>
+      <h1>Welcome to {APP_TITLE}!</h1>
+      <p>This would be probably the starting point of your app.</p>
+    </div>
+  );
+}
